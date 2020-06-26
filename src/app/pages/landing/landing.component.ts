2cee114ae390d9497268c8b7a703d595ac169615
David Mahecha <David.Mahecha@syntelli.com>
2020-06-26 00:21:15 -0500

    last change

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(
+      "%cIf you are here, you may want to contact valiztar@gmail.com",
+      "color: green; font-size:12px;"
+    );
   }
-
 }
