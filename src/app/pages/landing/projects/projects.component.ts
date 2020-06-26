36357081b2e09ae3923a7a9c8c727b9f92f47edd
David Mahecha <David.Mahecha@syntelli.com>
2020-06-26 00:08:38 -0500

    refactor

--- src/app/pages/landing/projects/projects.component.ts
+++ src/app/pages/landing/projects/projects.component.ts
@@ -14,37 +14,7 @@ export class ProjectsComponent implements OnInit {
     divider: true,
   };
 
-  Projects: any; /* = [
-    {
-      mainImageURL: "/assets/img/Untitled.png",
-      logoImageURL: "/assets/Logos/diners.png",
-      title: "Sitio publico y privado",
-      description:
-        "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s",
-      tags: ["usabilidad", "ui", "ux", "test_con_usuarios"],
-      contrast: false,
-    },
-    {
-      mainImageURL: "/assets/img/Untitled2.png",
-      logoImageURL:
-        "https://userscontent2.emaze.com/images/ba55b900-01dd-42b5-8b6d-1d0d5418466c/635429732438867392_logoblanco.png",
-      title: "Sitio web 2017",
-      description:
-        "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s",
-      tags: ["responsive", "ui", "ux"],
-      contrast: false,
-    },
-    {
-      mainImageURL: "/assets/img/Untitled3.png",
-      logoImageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Copec_logo.svg",
-      title: "TV App",
-      description:
-        "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s",
-      tags: ["usabilidad", "ui", "ux", "test_con_usuarios"],
-      contrast: true,
-    },
-  ]; */
+  Projects: any;
   p: any;
 
   constructor(public projectsService: ProjectsService) {}
